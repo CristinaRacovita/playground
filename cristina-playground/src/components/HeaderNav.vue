284e28c4c19566a74fc15fdc15bbb69fe6fc2804
Cristina Racovita <cristina.racovita98@gmail.com>
2024-03-10 14:47:34 +0100

    remove scrollbar of browser

--- cristina-playground/src/components/HeaderNav.vue
+++ cristina-playground/src/components/HeaderNav.vue
@@ -1,10 +1,6 @@
 <template>
   <header class="bg-white dark:bg-dark-indigo rounded-b-lg shadow-md">
     <div class="flex justify-between items-center p-3">
-    <!-- <div>
-      <img class="w-56" src="@/assets/two-clouds.png" />
-    </div> -->
-    
       <nav class="flex flex-grow justify-evenly px-52" aria-label="Global">
         <a href="#" class="text-ml font-normal hover:font-bold leading-6 text-sky-blue dark:text-white text-increase-on-hover">About Me</a>
         <a href="#" class="text-ml font-normal hover:font-bold leading-6 text-sky-blue dark:text-white text-increase-on-hover">My Projects</a>
